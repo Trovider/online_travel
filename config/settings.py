6cd90428e255e1cfc137731dbf912cdfbe81164b
dd6679 <35446851+dd6679@users.noreply.github.com>
2021-05-14 04:01:02 +0900

    admin 추가, 모델 migrate

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'online_travel.apps.OnlineTravelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- online_travel/admin.py
+++ online_travel/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Member
+from .models import Spot
+from .models import Video
+from .models import Livechat
+from .models import Bookmark
+
+admin.site.register(Member)
+admin.site.register(Spot)
+admin.site.register(Video)
+admin.site.register(Livechat)
+admin.site.register(Bookmark)
