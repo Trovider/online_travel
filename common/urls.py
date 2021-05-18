29a9a6f97e3252186f7b15f04fc8e5f969a16997
kimnakyung <dd6679@naverc,om>
2021-05-19 02:55:26 +0900

    회원가입

--- common/urls.py
+++ common/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'common'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('select-country/', views.select_country, name='select-country'),
+    path('select-area/', views.select_area, name='select-area'),
+    path('bookmark/', views.bookmark, name='bookmark'),
+    path('tourpage/', views.tourpage, name='tourpage'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from common.forms import UserForm
 
-# Create your views here.
+
+def signup(request):
+    """
+    계정생성
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
+
+
+def select_country(request):
+    return render(request, 'common/select-country')
+
+def select_area(request):
+    return render(request, 'common/select-area')
+
+def bookmark(request):
+    return render(request, 'common/bookmark')
+
+def tourpage(request):
+    return render(request, 'common/tourpage')
+# ---------------------------------------------------------------------------- #
